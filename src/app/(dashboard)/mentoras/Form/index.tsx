b3b35022fa90efbac20ec4e0e30090f2c380c61c
gustavo-bueno <gustavobcarvalho22@gmail.com>
2025-02-12 20:42:20 -0300

    feat: integrate mentor with api

--- src/app/(dashboard)/mentoras/Form/index.tsx
+++ src/app/(dashboard)/mentoras/Form/index.tsx
@@ -3,8 +3,12 @@
 import Button from "@/app/components/Button";
 import DeleteButton from "@/app/components/DeleteButton";
 import Input from "@/app/components/Input";
+import { deleteMentor, inviteMentor } from "@/app/services/mentors";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import * as yup from "yup";
 
 const mentorSchema = yup.object().shape({
@@ -21,15 +25,53 @@ type FormProps = {
   mentors: any[];
 };
 
-const Form = ({ mentors }: FormProps) => {
+const Form = ({ mentors: initialMentors }: FormProps) => {
+  const [mentors, setMentors] = useState(initialMentors);
+  const [loading, setLoading] = useState(false);
+  const { data: session } = useSession();
   const mentorForm = useForm<MentorForm>({
     resolver: yupResolver(mentorSchema),
   });
 
-  console.log(mentors);
-
-  const onSubmit = async () => {};
+  const token = session?.user.token || "";
   const { errors } = mentorForm.formState;
+
+  const onSubmit = async (data: MentorForm) => {
+    setLoading(true);
+    try {
+      const { success, ...mentor } = await inviteMentor(
+        {
+          nome: data.name,
+          email: data.email,
+        },
+        token,
+      );
+
+      if (success) {
+        toast.success("Mentora convidada com sucesso!");
+        setMentors((currentMentors) => [...currentMentors, mentor]);
+        mentorForm.reset();
+      }
+    } catch {
+      toast.error("Erro ao convidar mentora");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteUser = async (id: string) => {
+    try {
+      const result = await deleteMentor(id, token);
+      if (result.success) {
+        toast.success("Mentora apagada com sucesso!");
+        setMentors((currentMentors) =>
+          currentMentors.filter((mentor) => mentor._id !== id),
+        );
+      }
+    } catch {
+      toast.error("Erro ao apagar mentora.");
+    }
+  };
 
   return (
     <FormProvider {...mentorForm}>
@@ -50,7 +92,9 @@ const Form = ({ mentors }: FormProps) => {
             name="email"
             error={errors.email && errors.email?.message}
           />
-          <Button className="min-w-[298px]">Criar mentora</Button>
+          <Button loading={loading} className="min-w-[298px]">
+            Criar mentora
+          </Button>
         </form>
         <ul className="flex flex-col gap-[36px]">
           {mentors.map((mentor) => (
@@ -59,7 +103,7 @@ const Form = ({ mentors }: FormProps) => {
                 <p className="font-bold text-[20px]">{mentor.nome}</p>
                 <p>{mentor.email}</p>
               </div>
-              <DeleteButton onClick={() => console.log("delete")} />
+              <DeleteButton onClick={() => deleteUser(mentor._id)} />
             </li>
           ))}
         </ul>
--- src/app/services/mentors.ts
+++ src/app/services/mentors.ts
@@ -16,3 +16,41 @@ export const getMentors = async (token: string) => {
 
   return mentors.filter((mentor: { admin: boolean }) => !mentor.admin);
 };
+
+export const inviteMentor = async (
+  body: {
+    nome: string;
+    email: string;
+  },
+  token: string,
+) => {
+  const mentor = await api<{
+    objUsuario: Mentora;
+  }>({
+    path: "api/usuarios",
+    method: "POST",
+    body: {
+      ...body,
+      senha: "0",
+      admin: false,
+    },
+    token,
+  });
+
+  if (mentor?.objUsuario?._id) return { success: true, ...mentor.objUsuario };
+
+  return { success: false };
+};
+
+export const deleteMentor = async (id: string, token: string) => {
+  const mentor = await api<{
+    msg: string;
+  }>({
+    path: `api/usuarios/${id}`,
+    method: "DELETE",
+    token,
+  });
+
+  if (mentor?.msg === "Usuário deletado!") return { success: true };
+  return { success: false };
+};
